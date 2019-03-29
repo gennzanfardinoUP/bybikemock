49bb7f33e673eddfaed72968a4bfea56a7d39755
BradS0 <Bradley.Onyett@gmail.com>
2019-03-29 18:42:34 +0000

    Added button functionality that redirects to other pages. (Said pages don't exist yet)
    Created util.js file for basic js usage.

---
+++ INSE Prototype/Team3D/util.js
@@ -0,0 +1,18 @@
+function favouritesRedirect {
+  window.location.href("favourites.html");
+}
+
+function bookmarksRedirect {
+  window.location.href("bookmarks.html");
+}
+
+function photosRedirect {
+  window.location.href("photos.html");
+}
+
+function placesRedirect {
+  window.location.href("places.html");
+}
+
+/* Not sure if this is needed,
+it depends on how we implement the above pages. */
